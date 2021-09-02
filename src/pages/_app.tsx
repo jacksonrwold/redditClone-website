5a33bd44db126ff97df1beb02d6e3353c2bffb65
Jackson Wold <jacksonrwold@gmail.com>
2021-09-01 19:34:40 -0500

    returning to project and commiting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,29 @@
-import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
+import { ThemeProvider, CSSReset, ColorModeProvider } from '@chakra-ui/react'
+import { Provider, createClient } from 'urql'
+
+const client = createClient({ 
+  url: "http://localhost:4000/graphql",
+  fetchOptions: {
+    credentials: "include"
+  } 
+})
 
 import theme from '../theme'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <ColorModeProvider
-        options={{
-          useSystemColorMode: true,
-        }}
-      >
-        <Component {...pageProps} />
-      </ColorModeProvider>
-    </ChakraProvider>
+    <Provider value={client}>
+      <ThemeProvider theme={theme}>
+        <ColorModeProvider
+          options={{
+            useSystemColorMode: true,
+          }}
+        >
+          <CSSReset />
+          <Component {...pageProps} />
+        </ColorModeProvider>
+      </ThemeProvider>
+    </Provider>
   )
 }
 
