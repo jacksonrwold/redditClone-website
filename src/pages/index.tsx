d006c48d594548f1aa8f9482106616e00867fdcc
Jackson Wold <jacksonrwold@gmail.com>
2021-09-09 16:41:51 -0500

    added a bunch of things

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,18 @@
+import { withUrqlClient } from "next-urql";
 import { NavBar } from "../components/NavBar";
+import { usePostsQuery } from "../generated/graphql";
+import { createUrqlClient } from "../utils/createUrqlClient";
 
-const Index = () => (
-  <>
+const Index = () => {
+  const [{data}] = usePostsQuery()
+  
+  return (<>
     <NavBar />
     <div>hello world</div>
+    {/* @ts-ignore */}
+    {!data ? <div>Loading...</div> : data.posts.map((p) => <div key={p.id}>{p.title}</div>)}
   </>
-);
+  )
+};
 
-export default Index;
+export default withUrqlClient(createUrqlClient)(Index);
