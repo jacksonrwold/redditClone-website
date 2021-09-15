73268d7d4078d61f7f71bb5ffa818ed9e47b40ea
Jackson Wold <jacksonrwold@gmail.com>
2021-09-14 23:38:08 -0500

    added basic pagination functionality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,12 @@ import { usePostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import { Box, Button, Flex, Heading, Link, Stack, Text } from "@chakra-ui/core";
 import NextLink from "next/link";
+import { useState } from "react";
 
 const Index = () => {
+  const [variables, setVariables] = useState({limit: 10, cursor: null as null | string})
   const [{data, fetching}] = usePostsQuery({
-    variables: {
-      limit: 10
-    }
+    variables
   })
   
   if (!fetching && !data)  {
@@ -39,7 +39,12 @@ const Index = () => {
       )}
       { data ? (
       <Flex>
-        <Button isLoading={fetching} m="auto" my={8}>load more</Button>
+        <Button onClick={() => {
+          setVariables({
+            limit: variables.limit,
+            cursor: data.posts[data.posts.length - 1].createdAt
+          })
+        }} isLoading={fetching} m="auto" my={8}>load more</Button>
       </Flex>
       ) : null }
     </Layout>
